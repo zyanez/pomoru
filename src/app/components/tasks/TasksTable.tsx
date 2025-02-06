6d5ce3bbfd7b7fabca4fd52c3cdd5f967896e2c4
Luaguilar <64274701+Luaguilar@users.noreply.github.com>
2025-02-06 00:13:17 -0300

    ✨ Feature : Create and update functionality for tasks
    +Tasks area created through a modal, almost the same as project modal
    +Tasks can be updated through special method, is only used when changeing to completed with a checkbox, thinking to change it to a button

--- src/app/components/tasks/TasksTable.tsx
+++ src/app/components/tasks/TasksTable.tsx
@@ -1,15 +1,19 @@
 import { CirclePlus, Loader2 } from "lucide-react";
 import { useEffect, useState } from "react";
 import { Task } from "../../types/utils";
+import { TaskDetails } from "./TaskDetails";
+import CreateTaskModal from "../CreateTaskModal";
 
 const initial_list : Task[] = []
 
 export function TasksTable({selectedProjectId}:{selectedProjectId : number}){
     const [taskList, setTaskList] = useState(initial_list)
     const [loading, setLoading] = useState(true)
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     useEffect(() => {
         const fetchTasks = async () => {
+            setLoading(true)
             try {
                 const response = await fetch("/api/tasks/"+selectedProjectId);
                 if (!response.ok) {
@@ -29,18 +33,18 @@ export function TasksTable({selectedProjectId}:{selectedProjectId : number}){
         fetchTasks();
     }, [selectedProjectId]);
 
-    const handleClick = async () => {
+    const handleTaskCreated = (newTask: Task) => {
+        setTaskList(taskList.concat(newTask));
+    };
+
+    const updateTask = async (task:Task) => {
         try {
             const response = await fetch("/api/tasks", {
-                method: "POST",
+                method: "PUT",
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify({
-                        title: "TEST",
-                        important: 1,
-                        projectId: selectedProjectId
-                }),
+                body: JSON.stringify(task),
             });
     
             if (!response.ok) {
@@ -48,10 +52,16 @@ export function TasksTable({selectedProjectId}:{selectedProjectId : number}){
             }
     
             const newTask = await response.json();
-            console.log("Project created successfully:", newTask);
-            alert("CORRECTO");
-            setTaskList(taskList.concat(newTask))
-            //onProjectCreated(newTask);
+            alert("no error")
+            setTaskList(taskList.map((oldTask, i) => {
+                if (oldTask.id === newTask.id) {
+                  // Replace with new task
+                  return newTask;
+                } else {
+                  // The rest does not change
+                  return oldTask;
+                }
+              }))
         } catch (error) {
             console.error("Error creating task:", error);
             alert("There was an error creating the task. Please try again.");
@@ -60,10 +70,7 @@ export function TasksTable({selectedProjectId}:{selectedProjectId : number}){
 
     
 
-    // fetch tasks related to project
-    // need CRUD for tasks
-    // create/POST needs button somewhere
-    // others RUD need special row component TaskDetails
+
     return <>
         {loading ? (
             <div className="flex items-center justify-center h-16">
@@ -74,15 +81,27 @@ export function TasksTable({selectedProjectId}:{selectedProjectId : number}){
             </div>
         ) : (
             <div>
-                <button onClick={handleClick}>
+                <button onClick={()=>setIsModalOpen(true)}>
                     <CirclePlus className="text-black h-4 w-4"/>
                 </button>
                 {
                     taskList.length != 0 ? taskList.map((task, i) => (
-                        <p className="text-black" key={i}>{task.id}</p>
+                        <div className="flex flex-row">
+                            <input type="checkbox" checked={task.completed} onChange={()=>{
+                                task.completed = true;
+                                updateTask(task)
+                            }} />
+                            <span className={"mx-2 text-bold " + (task.important == 1 ? "text-green-500" : "text-red-500")}>{task.title}</span>
+                        </div>
                     )) : <p className="text-black">no task</p>
                 }
             </div>
         )}
+        <CreateTaskModal
+            isOpen={isModalOpen}
+            onClose={() => setIsModalOpen(false)}
+            onTaskCreated={handleTaskCreated}
+            projectId={selectedProjectId}
+        />
     </>
 }
