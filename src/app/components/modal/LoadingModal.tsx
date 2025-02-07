3a57a725f2f6a66838797589245b3a086169b5db
Luaguilar <64274701+Luaguilar@users.noreply.github.com>
2025-02-07 15:06:40 -0300

    🚧 Wip : new LoadingModal for async events
    Modal with the little loading effect, if footerAction is async. modal waits for it to finish, if returns true closes modal

---
+++ src/app/components/modal/LoadingModal.tsx
@@ -0,0 +1,74 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { X, Plus } from "lucide-react";
+import { useState } from "react";
+import BaseModal from "./BaseModal";
+
+interface LoadingModalProps {
+    title: string;
+    children: React.ReactNode;
+    confirmLabels: {
+        normalLabel: string;
+        loadingLabel: string;
+    };
+    onConfirm: () => Promise<boolean>;
+    buttonLabel?: string;
+    buttonIcon?: React.ReactNode;
+    onOpen?: () => void
+    onClose?: () => void
+}
+
+export default function LoadingModal({
+    title,
+    children,
+    confirmLabels = {
+        normalLabel: "Confirm",
+        loadingLabel: "Confirming..."
+    },
+    buttonLabel = "Open Modal",
+    buttonIcon = <Plus className="h-5 w-5 ml-3" />,
+    onConfirm,
+    onOpen,
+    onClose
+}: LoadingModalProps) {
+    const [isLoading, setIsLoading] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleClosing = () => {
+        onClose && onClose();
+        setIsOpen(false);
+    }
+
+    const handleOpening = () => {
+        onOpen && onOpen();
+        setIsOpen(true);
+    }
+
+    const handleConfirm = async () => {
+        setIsLoading(true);
+        const result = await onConfirm();
+        if (result) {
+            setIsLoading(false);
+            setIsOpen(false);
+        }
+        setIsLoading(false);
+    }
+
+    const footerAction = {
+        label : isLoading ? confirmLabels.loadingLabel : confirmLabels.normalLabel, 
+        onClick : handleConfirm
+    }
+
+    return (
+        <BaseModal
+            title={title} 
+            isOpen={isOpen} 
+            onOpen={handleOpening}
+            onClose={handleClosing}
+            buttonLabel={buttonLabel}
+            buttonIcon={buttonIcon}
+            footerAction={footerAction}
+        >
+            {children}
+        </BaseModal>
+    );
+}
