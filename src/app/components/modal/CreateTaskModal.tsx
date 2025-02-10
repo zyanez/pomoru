4ee2fc4866fe781e408a3249501fd00ad62a3df9
zyanez <zaheyanez@gmail.com>
2025-02-10 10:25:52 -0300

    🎨 Style: Improve Create Task Modal UI
    Renamed "Importance" to "Priority" and improved priority selection UI with more suitable buttons. ✨

--- src/app/components/modal/CreateTaskModal.tsx
+++ src/app/components/modal/CreateTaskModal.tsx
@@ -1,10 +1,8 @@
-
 // task is null -> insert
 // task is not null -> update/delete
 
-import { useMemo, useState } from "react";
-import BaseModal from "./BaseModal";
-import { ClipboardPlus, User, X } from "lucide-react";
+import { useState } from "react";
+import { AlertTriangle, Circle, CircleAlert, User,  } from "lucide-react";
 import { useTaskList } from "@/app/providers/taskList/use";
 import { useSelectedProject } from "@/app/providers/selectedProject/use";
 import LoadingModal from "./LoadingModal";
@@ -15,16 +13,17 @@ export function CreateTaskModal(){
     const [title, setTitle] = useState("");
     const [important, setImportant] = useState(0);
 
-    const importanceOptions: { text: string; color: keyof typeof colorStyles }[] = [
-        { text: "Low", color: "emerald" },
-        { text: "Medium", color: "amber" },
-        { text: "High", color: "rose" },
+    const importanceOptions: { text: string; color: keyof typeof colorStyles; icon: React.ReactNode }[] = [
+        { text: "Low", color: "emerald", icon: <Circle className="text-emerald-500 mr-2" size={20} /> },
+        { text: "Medium", color: "amber", icon: <CircleAlert className="text-amber-500 mr-2" size={20} /> },
+        { text: "High", color: "rose", icon: <AlertTriangle className="text-rose-500 mr-2" size={20} /> },
     ];
 
+
     const colorStyles = {
-        emerald: "bg-emerald-100 border-emerald-500 text-emerald-700",
-        amber: "bg-amber-100 border-amber-500 text-amber-700",
-        rose: "bg-rose-100 border-rose-500 text-rose-700",
+        emerald: "bg-emerald-100 border-emerald-400 text-emerald-500",
+        amber: "bg-amber-100 border-amber-400 text-amber-500",
+        rose: "bg-rose-100 border-rose-400 text-rose-500",
     };
 
     const createTask = async () => {
@@ -69,10 +68,9 @@ export function CreateTaskModal(){
     }
 
     const labels = {
-        normalLabel: "Create Task",
-        loadingLabel: "Creating Task..."
+        normalLabel: "Add Task",
+        loadingLabel: "Saving Task..."
     }
-
 
     return <LoadingModal
         title={"Create Task"}
@@ -87,7 +85,7 @@ export function CreateTaskModal(){
                 type="text"
                 value={title}
                 onChange={(e) => setTitle(e.target.value)}
-                placeholder="Enter task title"
+                placeholder="Enter Task Title"
                 className="w-full text-black p-2 border rounded border-slate-200 focus:outline-none focus:ring-4 focus:ring-slate-50 transition-all duration-200"
                 autoFocus
             />
@@ -107,7 +105,7 @@ export function CreateTaskModal(){
                             important === i ? ` ${colorStyles[option.color]} ` : "bg-white border-slate-300 text-slate-700 hover:bg-slate-50"
                         }`}
                     >
-                        <User size={18} className="mr-2" />
+                        {option.icon}
                         {option.text}
                     </button>
                 ))}
