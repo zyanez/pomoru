db2ab72d82e7536e365ef46956272506103cc259
zyanez <zaheyanez@gmail.com>
2025-02-12 19:45:09 -0300

    🎨 Style: New "Logo"

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,26 +1,20 @@
 export function Logo({ className = "w-6 h-6" }: { className?: string }) {
     return (
         <svg
-            viewBox="0 0 40 40"
+            viewBox="0 0 100 100"
             className={className}
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
         >
-            <ellipse
-                cx="20"
-                cy="20"
-                rx="15"
-                ry="10"
+            <circle
+                cx="50"
+                cy="50"
+                r="40"
                 stroke="currentColor"
-                strokeWidth="4"
+                strokeWidth="10"
                 fill="none"
             />
-            <circle
-                cx="20"
-                cy="20"
-                r="5"
-                fill="currentColor"
-            />
+            <circle cx="50" cy="50" r="15" fill="currentColor" />
         </svg>
     );
 }
