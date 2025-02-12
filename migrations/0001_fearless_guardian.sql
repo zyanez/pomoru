4c758f12922584fbfc7d2de878790e90c86b996e
Zabdiel Yanez <165136443+zyanez@users.noreply.github.com>
2025-02-11 21:18:32 -0300

    🔥 Remove: Unnecessary files

--- migrations/0001_fearless_guardian.sql
+++
@@ -1,3 +0,0 @@
-DROP INDEX "users_email_unique";--> statement-breakpoint
-ALTER TABLE `projects` ALTER COLUMN "created_at" TO "created_at" text NOT NULL DEFAULT (current_timestamp);--> statement-breakpoint
-CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);
